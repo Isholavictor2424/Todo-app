2bca6745e0728ebd85b5a8f5ea8ddeb02c4ae71d
Your Name <youremail@example.com>
2025-10-23 19:21:09 +0100

    Refactor Todo app: Update HTML structure, enhance user greeting functionality, and remove unused JavaScript. Add loading animation and style improvements.

--- page.js
+++ page.js
@@ -1,7 +1,12 @@
 
-
+const name = localStorage.getItem("username");
+$("#helloName").text(`Hello, ${name}!😊`);
 function addTodo() {
     const todoInput = $("#todo-input").val();
+    if (todoInput.trim() === "") {
+        alert("Please enter a todo item.");
+        return;
+    }
     let deleteIcon = `<img class="delete" src="delete.png" style="height: 20px; margin-left: 10px;" alt="">`;
     let icon = ` <img class= "checkbox" src="uncheck.png" style="height: 20px;" alt="">`
     let todoCard = `<li class = "todo-card">${icon} ${todoInput} ${deleteIcon}</li>`;
@@ -10,6 +15,15 @@ function addTodo() {
         $("#todo-input").val('');
     }
 }
+
+function deleteTodo() {
+    $(document).on("click", ".delete", function () {
+        $(this).parent().remove();
+    });
+}
+
+deleteTodo();
+
 
 $("#todo-form").on("submit", function (event) {
     event.preventDefault();
@@ -34,4 +48,7 @@ $(document).on("click", ".checkbox", function () {
     } else {
         $(this).attr("src", "uncheck.png");
     }
+})
+$("#reset").click(function(){
+    $("#todo-list").empty();
 })
